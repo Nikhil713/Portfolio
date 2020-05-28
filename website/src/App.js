c737ad2e03d9f0b6b51e3a6bf28a3d36d44db7d7
Nikhil713 <nikhilbabu213@gmail.com>
2020-05-28 23:41:36 +0530

    Add home and nav:

--- website/src/App.js
+++ website/src/App.js
@@ -9,7 +9,7 @@ export default class App extends React.Component {
   render() {
       return(
         <div>
-          <Navbar />
+          {/* <Navbar /> */}
           <Home />
         </div>
       )
--- website/src/pages/home/home.js
+++ website/src/pages/home/home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './home.css'
 
 
 export default class Home extends React.Component {
     render() {
         return (
-            <div>
+            <div className="home">
                 HEllo i am Nikhil
             </div>
         )
