c9a44f5449d4ddb0bd31f84bc74422b6d1940f58
Nikhil713 <nikhilbabu213@gmail.com>
2020-05-30 20:58:30 +0530

    Add profile img

--- website/src/pages/home/home.js
+++ website/src/pages/home/home.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import './home.css'
-
+import profile from '../../assets/Nikhil.jpg'
 
 export default class Home extends React.Component {
     render() {
         return (
             <div className="home">
-                HEllo i am Nikhil
+                <div className="bg-home">
+                    <div className="bg-home-left"></div>
+                    <div className="bg-home-right"></div>
+                </div>
+                <div className="home-info">
+                    <div className="home-card">
+                        <div className="card-img">
+                            <img src={profile} alt="Profile-picture"></img>
+                        </div>
+                    </div>
+                    <div className="home -info">
+
+                    </div>
+                </div>
             </div>
         )
     }
