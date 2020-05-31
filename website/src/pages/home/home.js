3ffd03413d32596e5c8cf52cc1adf30dbddbff08
Nikhil713 <nikhilbabu213@gmail.com>
2020-05-31 19:49:14 +0530

    Finish home layout

--- website/src/pages/home/home.js
+++ website/src/pages/home/home.js
@@ -23,9 +23,23 @@ export default class Home extends React.Component {
                                 <h3>PROJECT MANAGER</h3>
                             </div>
                         </div>
-                        <div className="home -info">
+                        <div className="home-side-info">
                             <div className="info-head">
                                 <h1>Hello!</h1>
+                            </div>
+                            <div className="info-subheader">
+                                Here's who I am & what I do
+                            </div>
+                            <div className="buttons">
+                                <button>A</button>
+                                <button>B</button>
+                            </div>
+                            <div className="info-details">
+                                I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.
+
+                                
+
+                                I’m a great place for you to tell a story and let your users know a little more about you.
                             </div>
                         </div>
                     </div>
