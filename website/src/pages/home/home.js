2933a4973009d9ecd9ea6e195226f421399fee55
Nikhil713 <nikhilbabu213@gmail.com>
2020-06-01 23:43:43 +0530

    Almost there

--- website/src/pages/home/home.js
+++ website/src/pages/home/home.js
@@ -35,7 +35,7 @@ export default class Home extends React.Component {
                                 <button className="info-b2">PROJECTS</button>
                             </div>
                             <div className="info-details">
-                                I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.
+                                <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.</p>
                                 I’m a great place for you to tell a story and let your users know a little more about you.
                             </div>
                         </div>
