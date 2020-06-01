15fc9e58c94b22cb990b53b94a011d161d42df7e
Nikhil713 <nikhilbabu213@gmail.com>
2020-06-01 20:56:42 +0530

    Update fonts

--- website/src/pages/home/home.js
+++ website/src/pages/home/home.js
@@ -28,17 +28,14 @@ export default class Home extends React.Component {
                                 <h1>Hello!</h1>
                             </div>
                             <div className="info-subheader">
-                                Here's who I am & what I do
+                                <h2>Here's who I am & what I do</h2>
                             </div>
-                            <div className="buttons">
-                                <button>A</button>
-                                <button>B</button>
+                            <div className="info-buttons">
+                                <button className="info-b1">RESUME</button>
+                                <button className="info-b2">PROJECTS</button>
                             </div>
                             <div className="info-details">
                                 I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.
-
-                                
-
                                 I’m a great place for you to tell a story and let your users know a little more about you.
                             </div>
                         </div>
