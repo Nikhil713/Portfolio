1c49f8f212c310ca5ffbfdf79b98a387bad9b204
Nikhil713 <nikhilbabu213@gmail.com>
2020-06-03 12:43:05 +0530

    Layout navbar

--- website/src/components/navbar/navbar.js
+++ website/src/components/navbar/navbar.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import './navbar.css'
 
-export default class Navbar extends React.Component{
-    render(){
-        return(
+export default class Navbar extends React.Component {
+    render() {
+        return (
             <div className="navbar-container">
-                
+                <nav>
+                    <div className="nav-left">
+                        <div className="blue-square"></div>
+                        <div className="nav-name"> Nikhil Babu</div>
+                        <div className="nav-details">/ PROJECT MANAGER</div>
+                    </div>
+                    <div className="nav-right">
+                        <div className="blue-square"></div>
+                        <div className="nav-name"> Nikhil Babu</div>
+                        <div className="nav-details">/ PROJECT MANAGER</div>
+                    </div>
+                </nav>
             </div>
         )
     }
