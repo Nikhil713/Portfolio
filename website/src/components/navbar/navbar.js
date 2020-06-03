e513fa3689de9d3c71c8966c8ac5defb290e701d
Nikhil713 <nikhilbabu213@gmail.com>
2020-06-03 23:37:43 +0530

    Add navbar-desktop

--- website/src/components/navbar/navbar.js
+++ website/src/components/navbar/navbar.js
@@ -7,18 +7,24 @@ export default class Navbar extends React.Component {
             <div className="navbar-container">
                 <nav>
                     <div className="nav-left">
-                        <div className="blue-square"></div>
-                        <div className="nav-name">
-                             <h1>Nikhil Babu</h1>
-                             <h2>/ PROJECT MANAGER</h2>
+                        <div className="nav-left-content">
+                            <div className="blue-square"></div>
+                            <div className="nav-name">
+                                <h1>Nikhil Babu</h1>
+                                <h2>/ PROJECT MANAGER</h2>
+                            </div>
                         </div>
-                        
                     </div>
                     <div className="nav-right">
-                        <div className="blue-square"></div>
-                        <div className="nav-name"> Nikhil Babu</div>
-                        <div className="nav-details">/  PROJECT MANAGER</div>
+                        <ul className="nav-links">
+                            <li className="menu-item">HOME</li>
+                            <li className="menu-item">RESUME</li>
+                            <li className="menu-item">PROJECTS</li>
+                            <li className="menu-item">CERTIFICATIONS</li>
+                            <li className="menu-item">CONTACT</li>
+                        </ul>
                     </div>
+                    
                 </nav>
             </div>
         )
