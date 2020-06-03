83cf8aba4f4e08b7262512c083902ea2d1035da0
Nikhil713 <nikhilbabu213@gmail.com>
2020-06-03 20:10:22 +0530

    Finish left side of nav

--- website/src/components/navbar/navbar.js
+++ website/src/components/navbar/navbar.js
@@ -8,13 +8,16 @@ export default class Navbar extends React.Component {
                 <nav>
                     <div className="nav-left">
                         <div className="blue-square"></div>
-                        <div className="nav-name"> Nikhil Babu</div>
-                        <div className="nav-details">/ PROJECT MANAGER</div>
+                        <div className="nav-name">
+                             <h1>Nikhil Babu</h1>
+                             <h2>/ PROJECT MANAGER</h2>
+                        </div>
+                        
                     </div>
                     <div className="nav-right">
                         <div className="blue-square"></div>
                         <div className="nav-name"> Nikhil Babu</div>
-                        <div className="nav-details">/ PROJECT MANAGER</div>
+                        <div className="nav-details">/  PROJECT MANAGER</div>
                     </div>
                 </nav>
             </div>
