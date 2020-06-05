cbb3c8176c27b15ecb122be15ccd4f06bb647874
Nikhil713 <nikhilbabu213@gmail.com>
2020-06-05 22:06:11 +0530

    Footer - initial commit

--- website/src/components/footer/footer.js
+++ website/src/components/footer/footer.js
@@ -2,11 +2,49 @@ import React from 'react'
 
 import './footer.css'
 
+import fb from '../../assets/fb.png'
+import github from '../../assets/github.png'
+import linkedin from '../../assets/linkedin.png'
+import gmail from '../../assets/gmail.png'
+
 export default class Footer extends React.Component{
     render(){
         return(
-            <div>
-                FOOTER
+            <div className="footer-container">
+                <footer>
+                    <div className="footer-left">
+                    
+                        <p>© 2020 by Nikhil Babu</p>
+                        <p>Created with React JS</p>
+                    </div>
+                    <div className="footer-right">
+                        <div className="call">
+                            <h1>Call</h1>
+                            <a href="tel:+918113805051" style={{textDecoration:"none", color:"black"}}>+91 8113805051</a>
+                        </div>
+                        <div className="write">
+                            <h1>Write</h1>
+                            <a href="mailto:nikhilbabu213@gmail.com" style={{textDecoration:"none", color:"black"}}>nikhilbabu213@gmail.com</a>
+                        </div>
+                        <div className="follow">
+                            <h1>Follow</h1>
+                            <div className="follow-icons">
+                            <a href="https://www.facebook.com/profile.php?id=100008338440903" target="blank" >
+                                    <img src={fb} alt="fb-logo" className="fb"></img>
+                                </a>
+                                <a href="https://github.com/Nikhil713" target="blank" >
+                                    <img src={github} alt="github-logo" className="github"></img>
+                                </a>
+                                <a href="https://www.linkedin.com/in/nikhil713/" target="blank" >
+                                    <img src={linkedin} alt="linkedin-logo" className="linkedin"></img>
+                                </a>
+                                <a href="mailto:nikhilbabu213@gmail.com" target="blank">
+                                    <img src={gmail} alt="gmail-logo" className="gmail"></img>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
             </div>
         )
     }
