1fa07058fb8d3365b881ee52bdd945a361744a21
AlexCallahan713 <alexcallahan9@gmail.com>
2020-06-10 11:14:18 +0530

    Finish resume card- pc view

--- website/src/components/experience-card/experience-card.js
+++ website/src/components/experience-card/experience-card.js
@@ -9,7 +9,7 @@ export default class Experience extends React.Component {
                     <h1>{this.props.exp.duration}</h1>
                     <h2>{this.props.exp.position}</h2>
                     <h3>{this.props.exp.company}</h3>
-                    <h3>{this.props.exp.location}</h3>
+                    <h3 style={{margin:"5px 0"}}>{this.props.exp.location}</h3>
                 </div>
                 <div className={styles["card-right"]}>
                     <p>{this.props.exp.desc1}</p>
