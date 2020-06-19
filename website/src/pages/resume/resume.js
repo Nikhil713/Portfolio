44b7b41212cdc95c07243e281fe40bc6eddfd536
Nikhil713 <nikhilbabu213@gmail.com>
2020-06-19 17:27:42 +0530

    Add html certificate

--- website/src/pages/resume/resume.js
+++ website/src/pages/resume/resume.js
@@ -3,7 +3,8 @@ import styles from './style.module.css'
 import Experience from '../../components/experience-card/experience-card'
 import Certificate from '../../components/certificate-cards/certificate'
 
-import image from '../../assets/coursera.png'
+import coursera from '../../assets/coursera.png'
+import linkedin from '../../assets/linkedinlogo.webp'
 import resume from '../../assets/Nikhil_Resume.pdf'
 
 export default class Resume extends React.Component {
@@ -15,8 +16,9 @@ export default class Resume extends React.Component {
                 { duration: "Apr 2018 - May 2018", position: "Intern", company: "FACT", location: "Cochin",desc1: "Learned about the ERP - SAP and how it can be used for resource planning and management",desc2: "Attained an insight as to how different sectors of a company can be integrated with the help of an ERP",tech:"SAP, ABAP" }
             ],
             certificateItem: [
-                { name: "Google Cloud Platform Fundamentals: Core Infrastructure", org: "Google Cloud", issue: "Coursera", img: image , link: "https://www.coursera.org/account/accomplishments/certificate/JDJV9RCUUQ38"},
-                { name: "Introduction to Data Science in Python", org: "University of Michigan", issue: "Coursera", img: image , link: "https://www.coursera.org/account/accomplishments/certificate/BR9B3TT65TQT"}
+                { name: "Google Cloud Platform Fundamentals: Core Infrastructure", org: "Google Cloud", issue: "Coursera", img: coursera , link: "https://www.coursera.org/account/accomplishments/certificate/JDJV9RCUUQ38"},
+                { name: "Introduction to Data Science in Python", org: "University of Michigan", issue: "Coursera", img: coursera , link: "https://www.coursera.org/account/accomplishments/certificate/BR9B3TT65TQT"},
+                { name: "HTML Essential Training", org: "Linked In Learning", issue: "Linked In", img: linkedin , link: "https://drive.google.com/file/d/1JgV2sLpa_mkRXxR6_oHaH1gL7ccgF4uQ/view"}
             ]
         }
     }
